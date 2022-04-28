9f0b829592cdc94d178fba27dfb65e1ed13ddf7b
Daniel Mrva <mrva.paints@gmail.com>
2022-04-28 17:38:14 -0600

    filling out files, need to build models and routes next

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const reactionRoutes = require('./reactionRoutes');
+const thoughtRoutes = require('./thoughRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/reactions', reactionRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
