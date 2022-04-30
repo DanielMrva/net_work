843acc247bb0ccbf5cdc433fab2b4be88a500d2b
Daniel Mrva <mrva.paints@gmail.com>
2022-04-29 22:00:58 -0600

    forgot to export router from thoughtroutes, moving to controllers next

--- routes/api/thoughRoutes.js
+++ routes/api/thoughRoutes.js
@@ -14,3 +14,5 @@ router.route('/').get(getThoughts).post(createThought);
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
 
 router.route('/:thoughtId/reactions/:reactionId?').post(createReaction).delete(deleteReaction);
+
+module.exports = router;
