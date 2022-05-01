6c137bb1333503aa41a91f068bcfe06a96693d67
Daniel Mrva <mrva.paints@gmail.com>
2022-05-01 14:42:25 -0600

    minor typo fix, did not fix thought query issues

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thoughRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
 router.use('/thoughts', thoughtRoutes);
