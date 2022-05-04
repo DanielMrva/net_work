bc379cf7658aff24265b51e8af155b63da9f4086
Daniel Mrva <mrva.paints@gmail.com>
2022-05-03 23:02:39 -0600

    seeding works for users and thoughts, including associated reactions, however I can't seem to associate the thoughts with their appropriate users

--- utils/data.js
+++ utils/data.js
@@ -1,110 +1,191 @@
-// const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
-// const lorem = new LoremIpsum({
-//     sentencesPerParagraph: {
-//       max: 8,
-//       min: 4
-//     },
-//     wordsPerSentence: {
-//       max: 12,
-//       min: 2
-//     }
-// });
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 12,
+      min: 2
+    }
+});
 
-// const randomText = () => {
-//     const rndInt = Math.floor((Math.random() * 4) + 1) ;
-//     return lorem.generateSentences(rndInt);
-// };
+const randomText = () => {
+    const rndInt = Math.floor((Math.random() * 3) + 1) ;
+    return lorem.generateSentences(rndInt);
+};
 
-// // console.log(randomText);
-// // console.log(randomText.length);
 
-// const names = [
-//     'Narwal',
-//     'Vine',
-//     'Clever',
-//     'Dude',
-//     'Bro',
-//     'Musk',
-//     'Elon',
-//     'Alonzo',
-//     'Volcano',
-//     'Unicorn',
-//     'IceCream',
-//     'Monster',
-//     'Aqua',
-//     'Viking',
-//     'Professor',
-//     'AeyBeeSea',
-//     'Airman',
-//     'Wizard',
-//     'Barbarian',
-//     'Werewolf',
-//     'Finnish',
-//     'Slimy',
-//     'Medusa',
-//     'Worm',
-//     'Jones',
-//     'Coollastname',
-//     'Ostrich',
-//     'Ze',
-//     'Zechariah',
-//     'Zeek',
-//     'Dog',
-//     'Excited',
-//     'Sock',
-//     'Box',
-//     'Zen',
-//     'Zenith',
-//     'Zennon',
-//     'Zeph',
-//     'Blob',
-//     'Monkey',
-//     'Zhi',
-//     'Zhong',
-//     'Zhuo',
-//     'Zion',
-//     'Zishan',
-//     'Fox',
-//     'Zuriel',
-//     'Xander',
-//     'Jared',
-//     'Grace',
-//     'Alex',
-//     'Bibliophile',
-//     'Wonderer',
-//     'Smile',
-//     'Sarah',
-//     'HotDog',
-//     'Parker',
-//   ];
+const names = [
+    'Narwal',
+    'Vine',
+    'Clever',
+    'Dude',
+    'Bro',
+    'Musk',
+    'Elon',
+    'Alonzo',
+    'Volcano',
+    'Unicorn',
+    'IceCream',
+    'Monster',
+    'Aqua',
+    'Viking',
+    'Professor',
+    'AeyBeeSea',
+    'Airman',
+    'Wizard',
+    'Barbarian',
+    'Werewolf',
+    'Finnish',
+    'Slimy',
+    'Medusa',
+    'Worm',
+    'Jones',
+    'Coollastname',
+    'Ostrich',
+    'Ze',
+    'Zechariah',
+    'Zeek',
+    'Dog',
+    'Excited',
+    'Sock',
+    'Box',
+    'Zen',
+    'Zenith',
+    'Zennon',
+    'Zeph',
+    'Blob',
+    'Monkey',
+    'Zhi',
+    'Zhong',
+    'Zhuo',
+    'Zion',
+    'Zishan',
+    'Fox',
+    'Zuriel',
+    'Xander',
+    'Jared',
+    'Grace',
+    'Alex',
+    'Bibliophile',
+    'Wonderer',
+    'Smile',
+    'Sarah',
+    'HotDog',
+    'Parker',
+    'Smelt',
+    'Lightning',
+    'Robot',
+    'Minion',
+    'Kangaroo',
+    'Captain',
+    'Doctor',
+  ];
 
-// const email = [
-//     '@gmail.com',
-//     '@ilstu.edu',
-//     '@aol.com',
-//     '@usgs.gov',
-// ];
+const email = [
+    '@gmail.com',
+    '@ilstu.edu',
+    '@aol.com',
+    '@usgs.gov',
+    '@dhs.gov',
+    '@cu.edu',
+    '@iu.edu',
+    '@yahoo.com',
+    '@hotmail.com',
+    '@msn.com',
+    '@comast.net',
+    '@live.com',
+    '@fee.fr',
+    '@gmx.de',
+    '@web.de',
+    '@outlook.com',
+    '@att.net',
+    '@shaw.ca',
+    '@juno.com',
+    '@mac.com'
+];
 
-// const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// const getRandomEmail = () => {
-//     `${getRandomArrItem(names)}${getRandomArrItem(email)}`
-// };
+const getRandomEmail = () => {
+    return `${getRandomArrItem(names)}.${getRandomArrItem(names)}${getRandomArrItem(email)}`
+};
 
-// const getRandomUser = () => {
-//     `${getRandomArrItem(names)} ${getRandomArrItem(names)}`
-// };
+const getRandomUser = () => {
+    return `${getRandomArrItem(names)} ${getRandomArrItem(names)}`
+};
 
-// const getRandomText = (int) => {
-//     const results = [];
+const generateUserGroup = (int) => {
+    const userGroup = [];
+    for (let i = 0; i < int; i++) {
+        userGroup.push(
+            {
+                userName: getRandomUser(),
+                email: getRandomEmail(),
+            }
+        )
+    } 
+
+    return userGroup;
+};
+
+// const generateData = (int) => {
+
+//     let userGroup = [];
+//     let thoughts = [];
 //     for (let i = 0; i < int; i++) {
-//         results.push({
-//             thoughtText: randomText()
+//         userGroup.push(
+//             {
+//                 userName: getRandomUser(),
+//                 email: getRandomEmail(),
+//             }
+//         )
+//     }
+
+//     for (let j = 0; j < int; j++) {
+//         const randomInt = (Math.floor(Math.random() * 3))
+//         thoughts.push({
+//             thoughtText: randomText(),
+//             userName: getRandomArrItem(userGroup).userName,
+//             reactions: getReactions(randomInt)
 //         })
 //     }
+//     console.log(userGroup);
+//     console.log(thoughts[0]);
+//     return [userGroup, thoughts];
 // }
 
-// module.exports = {getRandomUser, getRandomEmail, randomText}
+const getThoughts = (int, userGroup) => {
+    const thoughts = [];
+    for (let i = 0; i < int; i++) {
+        const randomInt = (Math.floor(Math.random() * 3))
+        thoughts.push({
+            thoughtText: randomText(),
+            userName: getRandomArrItem(userGroup).userName,
+            reactions: getReactions(randomInt, userGroup)
+        })
+    } 
+
+    return thoughts;
+};
+
+const getReactions = (int, userGroup) => {
+    const reactions = [];
+    for (let i = 0; i < int; i++) {
+
+        reactions.push({
+            reactionBody: randomText(),
+            userName: getRandomArrItem(userGroup).userName,
+
+        })
+    } return reactions;
+};
+
+
+
+module.exports = {getRandomUser, getRandomEmail, generateUserGroup, getRandomArrItem, getThoughts, randomText, getReactions}
   
+
 
--- utils/seed.js
+++ utils/seed.js
@@ -1,19 +1,63 @@
-// const connection = require('../config/connection');
-// const { User, Thought } = require('../models');
-// const { getRandomUser, getRandomEmail, randomText } = require('./data');
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const {getRandomUser, getRandomEmail, generateUserGroup, getRandomArrItem, getThoughts, randomText, getReactions} = require('./data');
 
-// connection.on('error', (err) => err);
+const thoughtAssociator = function(text, user){
+    Thought.findOne({thoughtText: text})
+        .then((thought) => 
+            !thought
+                ? console.log(`could not find ${thought}`)
+                : User.findOneAndUpdate(
+                {userName: thought.userName},
+                {$addToSet: {thoughts: thought._id}},
+                {runValidators: true, new: true}
+            )
+            .then((user) => console.log(user))
+        )
+        .catch((err) => console.log(err));
+};
 
-// connection.once('open', async () => {
-//     console.log('connected')
+const thoughtAssociate = async function(text){
+    const thought = await Thought.findOne({thoughtText: text})
+    if (!thought) {
+        console.log('could not find thought')
+    } else {
+        const user = await User.findOneAndUpdate(
+            {userName: thought.userName},
+            {$addToSet: {thoughts: thought._id}},
+            {runValidators: true, new: true}
+        )
+        console.log(user);
+    }
+}
 
-//     await User.deleteMany({});
-//     await Thought.deleteMany({});
 
-//     const users = [];
+connection.on('error', (err) => err);
 
-//     for (let i = 0; i < 20; i++) {
-//         let userName = getRandomUser();
-//         let email = getRandomEmail();
-//     }
-// });
+connection.once('open', async () => {
+    console.log('connected')
+
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    let userGroup = generateUserGroup(10);
+
+    let thoughts = getThoughts(10, userGroup);
+
+
+    
+    await Thought.collection.insertMany(thoughts);
+    await User.collection.insertMany(userGroup);
+
+    async function associateThoughts(thoughts) {
+        for (let index = 0; index < thoughts.length; index++) {
+            const text = thoughts[index].thoughtText;
+            const user = thoughts[index].userName;
+            thoughtAssociator(text);
+        }
+    }
+    await associateThoughts(thoughts);
+    console.table(userGroup);
+    process.exit(0);
+
+});
